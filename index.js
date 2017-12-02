67e9ce4a95e760f1dbb937de6aa3b4330664b382
gsuxlzt <jami.carangan@gmail.com>
2017-12-02 21:41:08 +0800

    :hammer: transform create user to a new function.

--- index.js
+++ index.js
@@ -19,23 +19,12 @@ app.post("/user_info", function (req, res) {
 		res.send("User needs an id");
 	}
 	else {
-		let user = {}
-		let rand_year = Math.floor(Math.random()*13) + 1;
-		let rand_work = Math.floor(Math.random()*5);
-		let rand_educ = Math.floor(Math.random()*3);
-		let bool = Math.floor(Math.random()*2) === 0;
-		user.user_profile_creation_date = moment().subtract(rand_year,'years');
-		user.user_profile_id = req.body.id;
-		let user_background = {};
-		user_background.work = getWorkStatus(rand_work);
-		user_background.education = getEducation(rand_educ);
-		user_background.age = bool ? 24 : 26;
-		user_background.family = bool
-		user.user_background = user_background
+		createUser(req.body.id)
 		res.sendStatus(200)
-
 	}
+
 });
+
 
 
 // Facebook Webhook
@@ -94,21 +83,9 @@ function processPostback(event) {
         greeting = "Hi " + name + ". ";
       }
       var message = greeting;
+      createUser(senderId);
       sendMessage(senderId, {text: message});
     });
-    request({
-    	url: "/user_info",
-    	qs: {id: senderId},
-    	method: "POST",
-    }, function(error, response, body) {
-    	if (error) {
-    		console.log("Error" + error)
-    	}
-    	else {
-    		console.log(JSON.parse(body))
-    	}
-    })
-  }
 }
 
 // sends message to user
@@ -128,29 +105,45 @@ function sendMessage(recipientId, message) {
   });
 }
 
-
-	function getWorkStatus (num) {
-		switch (num) {
-			case 0:
-				return 'UNEMPLOYED'
-			case 1:
-				return 'OTHER_MICRO_VENDOR'
-			case 2:
-				return 'SARI_SARI_VENDOR'
-			case 3:
-				return 'MARKET_VENDOR'
-			case 4:
-				return 'EMPLOYEE'
-		}
+function createUser(id) {
+	let user = {}
+	let rand_year = Math.floor(Math.random()*13) + 1;
+	let rand_work = Math.floor(Math.random()*5);
+	let rand_educ = Math.floor(Math.random()*3);
+	let bool = Math.floor(Math.random()*2) === 0;
+	user.user_profile_creation_date = moment().subtract(rand_year,'years');
+	user.user_profile_id = req.body.id;
+	let user_background = {};
+	user_background.work = getWorkStatus(rand_work);
+	user_background.education = getEducation(rand_educ);
+	user_background.age = bool ? 24 : 26;
+	user_background.family = bool
+	user.user_background = user_background
 	}
+}
 
-	function getEducation (num) {
-		switch (num) {
-			case 0:
-				return 'NONE'
-			case 1:
-				return 'HIGH_SCHOOL'
-			case 2:
-				return 'COLLEGE'
-		}
+function getWorkStatus (num) {
+	switch (num) {
+		case 0:
+			return 'UNEMPLOYED'
+		case 1:
+			return 'OTHER_MICRO_VENDOR'
+		case 2:
+			return 'SARI_SARI_VENDOR'
+		case 3:
+			return 'MARKET_VENDOR'
+		case 4:
+			return 'EMPLOYEE'
 	}
+}
+
+function getEducation (num) {
+	switch (num) {
+		case 0:
+			return 'NONE'
+		case 1:
+			return 'HIGH_SCHOOL'
+		case 2:
+			return 'COLLEGE'
+	}
+}
