bc5778b01b70d45562836d4125585f54e0cdd8c9
gsuxlzt <jami.carangan@gmail.com>
2017-12-02 17:01:54 +0800

    :sparkles: change logs

--- index.js
+++ index.js
@@ -46,8 +46,8 @@ app.post("/webhook", function (req, res) {
 function processPostback(event) {
   var senderId = event.sender.id;
   var payload = event.postback.payload;
-  console.log(senderId)
-  console.log(payload)
+  console.log(event.sender)
+  console.log(event.postback)
 
   if (payload === "Greeting") {
     // Get user's first name from the User Profile API
