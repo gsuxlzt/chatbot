b8bfe2d5c0374ce43002c24abf54ec3e05b5acb0
gsuxlzt <jami.carangan@gmail.com>
2017-12-02 17:49:39 +0800

    :bug:

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ function sendMessage(recipientId, message) {
 function getFriendsList (id) {
 	console.log(id);
 	request({
-		url: 'https://graph.facebook.com/v2.11' + id + '/friendslists',
+		url: 'https://graph.facebook.com/v2.11' + id + '/friendlists',
 		qs: {
 			access_token: process.env.PAGE_ACCESS_TOKEN
 		},
